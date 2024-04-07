4d4cc16e9d678e8cc7c9748603bbf79f325c7f45
Elavarasan <contact@elavarasan.me>
2024-04-07 05:43:07 +0530

    (#2)upadte

--- components/Container.js
+++ components/Container.js
@@ -1,6 +1,9 @@
-import SEO from '@/components/Common/SEO'
-import BLOG from '@/blog.config'
-import PropTypes from 'prop-types'
+import SEO from '@/components/Common/SEO';
+import BLOG from '@/blog.config';
+import PropTypes from 'prop-types';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 const Container = ({ children, fullWidth, ...customMeta }) => {
   const meta = {
@@ -10,12 +13,16 @@ const Container = ({ children, fullWidth, ...customMeta }) => {
   }
   return (
     <>
+         
+
       <SEO meta={meta} />
       <main
         className={`m-auto flex-grow w-full transition-all ${
           !fullWidth ? 'max-w-2xl px-4' : 'px-4 md:px-24'
         }`}
       >
+           <Analytics />
+           <SpeedInsights/>
         {children}
       </main>
     </>
