66426d9e77d1c43a95f4e569991a160ab5368287
Elavarasan <contact@elavarasan.me>
2024-04-08 21:34:05 +0530

    Update

--- components/Post/SupaComments.js
+++ components/Post/SupaComments.js
@@ -23,7 +23,7 @@ const SupaComments = () => {
       <div
         id='comments'
        // data-url='localhost:3000'
-        data-url="https://elavarasan.me"
+        data-url="elavarasan.me"
         supabase-url={BLOG.comment.supaCommentsConfig.supabaseUrl}
         anon-key={BLOG.comment.supaCommentsConfig.supabaseAnonKey}
       ></div>
