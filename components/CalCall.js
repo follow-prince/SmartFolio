c6f1767b07dc06c33ad2aa80b27ebbd08b9c3343
Elavarasan <contact@elavarasan.me>
2024-04-22 23:58:23 +0530

    update - #2s

--- components/CalCall.js
+++ components/CalCall.js
@@ -20,8 +20,8 @@ export default function CalCall() {
   }, []);
 
   return (
-    <button className='p-2 hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer rounded-lg dark:text-gray-100' data-cal-namespace="" data-cal-link="elavarasan/15min" data-cal-config='{"layout":"month_view"}'>
-      <CalendarIcon className='h-5 w-5' />
+    <button className=' p-2 hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer rounded-lg dark:text-gray-100' >
+<img data-cal-namespace="" data-cal-link="elavarasan/15min" data-cal-config='{"layout":"month_view"}' src="https://em-content.zobj.net/source/telegram/386/calendar_1f4c5.webp"  className='h-5 w-5'/>
     </button>
   );
 }
