fb713b48272d1f65a63ae11fe19e218c95ff58b4
Prince <elavarasa.003@gmail.com>
2024-08-14 08:42:40 +0530

    Update .gitignore to include 'bun.lockb' and 'pnpm-lock.yaml' Update 'link' in blog.config.js to 'https://princey.tech' Increase 'postsPerPage' in blog.config.js to 25 Fix class names and order in SupaComments.js component

--- components/Post/SupaComments.js
+++ components/Post/SupaComments.js
@@ -18,12 +18,11 @@ const SupaComments = () => {
   return (
     <>
       {/* SupaComments share existing tailwind css styles, don't need to import an additional css file. */}
-      <div className='hidden mt-10 space-y-4 mt-2 rounded-full w-8 h-8 w-10 h-10' />
-      <div className='hidden animate-pulse space-x-4 flex-1 h-2 w-20 rounded grid-cols-3 gap-4 col-span-1 col-span-2' />
+      <div className='hidden w-8 w-10 h-8 h-10 mt-2 mt-10 space-y-4 rounded-full' />
+      <div className='flex-1 hidden w-20 h-2 grid-cols-3 col-span-1 col-span-2 gap-4 space-x-4 rounded animate-pulse' />
       <div
         id='comments'
-        // data-url='localhost:3000'
-        data-url={BLOG.link.split('/').slice(2)}
+        data-url={BLOG.link.split('/').slice(2) || 'localhost:3000'}
         supabase-url={BLOG.comment.supaCommentsConfig.supabaseUrl}
         anon-key={BLOG.comment.supaCommentsConfig.supabaseAnonKey}
       ></div>
