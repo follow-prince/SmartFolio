3fb444aecf5891584f4eb9a1147f33cc7c5fd46a
ELAVARASAN <105858216+follow-prince@users.noreply.github.com>
2025-04-10 07:26:53 +0530

    Refactor SupaComments component styles for improved layout and consistency

--- components/Post/SupaComments.js
+++ components/Post/SupaComments.js
@@ -18,12 +18,12 @@ const SupaComments = () => {
   return (
     <>
       {/* SupaComments share existing tailwind css styles, don't need to import an additional css file. */}
-      <div className='hidden w-10 h-10 mt-10 space-y-4 rounded-full' />
-      <div className='flex-1 hidden w-20 h-2 grid-cols-3 col-span-2 gap-4 space-x-4 rounded animate-pulse' />
+      <div className='hidden  space-y-4 mt-2 rounded-full w-8 h-8 ' />
+      <div className='hidden animate-pulse space-x-4 flex-1 h-2 w-20 rounded grid-cols-3 gap-4 col-span-1 ' />
       <div
         id='comments'
-        data-url={BLOG.link.split('/').slice(2) || 'localhost:3000'}
-
+        // data-url='localhost:3000'
+        data-url={BLOG.link.split('/').slice(2)}
         supabase-url={BLOG.comment.supaCommentsConfig.supabaseUrl}
         anon-key={BLOG.comment.supaCommentsConfig.supabaseAnonKey}
       ></div>
