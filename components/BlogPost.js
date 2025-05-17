2984dc2ca2151a39e2440965ce1f9d7d7d15e18b
ELAVARASAN <105858216+follow-prince@users.noreply.github.com>
2025-05-17 14:26:29 +0530

    image issue fixing

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -8,7 +8,13 @@ import FormattedDate from '@/components/Common/FormattedDate'
 const BlogPost = ({ post }) => {
   return (
     <motion.div>
-      <Link passHref href={`${BLOG.path}/${post.slug}`} scroll={false}>
+      <Link
+        passHref
+        href={`${BLOG.path}/${post.slug}`}
+        scroll={false}
+        className='relative group cursor-pointer'
+      >
+        <div class='absolute -inset-1 bg-gradient-to-r from-red-600 dark:from-gray-600 to-violet-600 dark:to-violet-50 rounded-lg blur-lg opacity-10 group-hover:opacity-50 transition duration-1000 group-hover:duration-200'></div>
         <article
           key={post.id}
           className='group flex flex-col overflow-hidden relative mb-5 md:mb-8 cursor-pointer rounded-xl p-5'
@@ -17,18 +23,24 @@ const BlogPost = ({ post }) => {
             fill
             alt={`${post.title}`}
             src={post?.page_cover}
+            loading="lazy"
+            priority={false}
             className='w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200'
           />
           <div className='hidden md:block md-cover absolute inset-0'></div>
           <div className='md:hidden sm-cover absolute inset-0'></div>
           <div className='relative mt-auto'>
             <header className='flex flex-col justify-between md:flex-row md:items-baseline'>
-              <h2 className='text-lg md:text-xl font-medium mb-2 text-black dark:text-gray-100'>{post.title}</h2>
+              <h2 className='text-lg md:text-xl font-medium mb-2 text-black dark:text-gray-100'>
+                {post.title}
+              </h2>
               <span className='text-color-fix font-light flex-shrink-0 text-gray-600 dark:text-gray-400'>
                 <FormattedDate date={post.date} />
               </span>
             </header>
-            <p className='font-light hidden md:block leading-8 text-gray-700 dark:text-gray-300'>{post.summary}</p>
+            <p className='font-light hidden md:block leading-8 text-gray-700 dark:text-gray-300'>
+              {post.summary}
+            </p>
             {/* w-4/5  */}
           </div>
         </article>
